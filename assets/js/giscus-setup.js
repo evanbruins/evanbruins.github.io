aa34d8a451e76e246db68b09d217f8636db09b9e
evanbruins <ecbruins@gmail.com>
2025-11-18 20:27:57 +0000

    Deploying to gh-pages from @ evanbruins/evanbruins.github.io@6786567a7c56c5320caf91d241e2b8195bb0ca14 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "evanbruins/evanbruins",
+    "data-repo": "evanbruins/evanbruins.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
